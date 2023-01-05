3d9e8e136014d66b1985627c08d81272e55fd15b
julydeath <manojkarajada.mk@gmail.com>
2023-01-05 15:46:53 +0530

    Tours was done

---
+++ src/Tour.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+function Tour({ id, image, info, price, name, removeTour }) {
+  const [readmore, setReadmore] = useState(false);
+  return (
+    <article className="single-tour">
+      <img src={image} alt={name} />
+
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">${price}</h4>
+        </div>
+        <p>
+          {readmore ? info : `${info.substring(0, 150)}...`}
+          <button onClick={() => setReadmore(!readmore)}>
+            {readmore ? "showless" : "showmore"}
+          </button>
+        </p>
+        <button className="delete-btn" onClick={() => removeTour(id)}>
+          Delete
+        </button>
+      </footer>
+    </article>
+  );
+}
+
+export default Tour;
