f57f295c5599abba0b5e9edcae1bbb857ac17dfb
julydeath <manojkarajada.mk@gmail.com>
2023-01-05 20:27:36 +0530

    Review project done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import Review from "./Review";
 
 function App() {
   return (
-    <div className="App">
-      <h2>Master Branch</h2>
-    </div>
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>Our Reviews</h2>
+          <div className="underline"></div>
+        </div>
+        <Review />
+      </section>
+    </main>
   );
 }
 
